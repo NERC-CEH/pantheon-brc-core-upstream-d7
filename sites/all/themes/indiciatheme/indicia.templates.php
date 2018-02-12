b8eb873468ef06d06e4bb7a8ff34087203ff8ae1
NERC\bire <bire@ceh.ac.uk>
2018-02-12 15:01:55 +0000

    Uploaded some more modules and themes.

---
+++ sites/all/themes/indiciatheme/indicia.templates.php
@@ -0,0 +1,6 @@
+<?php
+global $indicia_templates;
+$indicia_templates['two-col-50'] = '<div class="at-panel panel-display two-50 clearfix">' .
+    '<div class="region region-two-50-first"><div class="region-inner clearfix">{col-1}</div></div>' .
+    '<div class="region region-two-50-second"><div class="region-inner clearfix">{col-2}</div></div>' .
+'</div>';
